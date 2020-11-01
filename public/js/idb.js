be612083d707b05915f0e17103eae01cdc9d50f7
Hani Ghaderi <hanighaderi@Users-MacBook-Pro.local>
2020-11-01 11:31:45 -0800

    push all

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+let db;
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+//event listener on request object
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+// This function will be executed when the applicaiton is not online
+function saveRecord(record) {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+  transactionObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  const getAll = transactionObjectStore.getAll();
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          const transactionObjectStore = transaction.objectStore('new_transaction');
+          transactionObjectStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// are back online yet?
+window.addEventListener('online', uploadTransaction);
